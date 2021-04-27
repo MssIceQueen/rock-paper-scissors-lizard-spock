d4d5b00d62efa912b202e664db4f53eee5d19336
Lisa PR <l.prat.rivero@gmail.com>
2021-04-27 15:11:16 +0200

    added comments

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+// element to show the message of the outcome
 let message = document.getElementById('message');
+// element to show the score
 let score = document.getElementById('score');
+// to select all the buttons in the class selections
 let buttons = document.getElementsByClassName('selections');
+// element to assign a different function to the reset button
 let resetBtn = document.getElementById('reset');
+// element to update the score
 let winnerScores = [0, 0];
 
 //add event listeners to buttons
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', playGame);
 }
-resetBtn.addEventListener('click',restart);
+resetBtn.addEventListener('click', restart);
 
 function playGame(choice) {
-    // setup player's selection
+    // setup player's selection: get the text of the target that triggers the event
     let playerSelection = choice.target.innerText;
     // setup a random number to select for computer
     // selects a number between 0 and 1 (1 not-inclusive)
@@ -38,7 +43,7 @@ function playGame(choice) {
         computerSelection = 'Spock';
     }
 
-    //setup a function to compare winners and return result
+    //setup a function to check the winner and return result
     let result = checkWinner(playerSelection, computerSelection);
 
     function checkWinner(player, computer) {
@@ -79,32 +84,32 @@ function playGame(choice) {
     }
 
 //output scores to the DOM
-if (result === 'player') {
-    result += ' wins!';
-    //update score
-    winnerScores[0]++;
-}
+    if (result === 'player') {
+        result += ' wins!';
+        //update score
+        winnerScores[0]++;
+    }
 
-if (result === 'computer') {
-    result += ' wins!';
-    winnerScores[1]++;
-}
+    if (result === 'computer') {
+        result += ' wins!';
+        winnerScores[1]++;
+    }
 
-if (result === 'draw') {
-    result += ". It's a tie!"
-}
+    if (result === 'draw') {
+        result += ". It's a tie!"
+    }
 
 //output score into Score DIV
-score.innerHTML = 'Player: [ ' + winnerScores[0] + ' ] Computer: [ ' + winnerScores[1] + ' ]';
+    score.innerHTML = 'Player: [ ' + winnerScores[0] + ' ] Computer: [ ' + winnerScores[1] + ' ]';
 
 //output player and computer's selections
-messenger('Player: <strong>' + playerSelection + '</strong> Computer: <strong>' + computerSelection + '</strong><br>' + result);
+    messenger('Player: <strong>' + playerSelection + '</strong> Computer: <strong>' + computerSelection + '</strong><br>' + result);
 }
-
-function restart(){
-    winnerScores = [0,0];
+// function for the reset button
+function restart() {
+    winnerScores = [0, 0];
 }
-
+// to display the message
 function messenger(selectionMessage) {
     message.innerHTML = selectionMessage;
 }
